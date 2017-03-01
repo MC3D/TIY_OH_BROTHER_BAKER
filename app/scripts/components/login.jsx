f2ebc9fbee5e60c786f79d6ab1e92d59b56a1aba
MC3D <m.chapman.home@gmail.com>
2017-03-01 15:58:32 -0500

    adds login and signup methods

--- app/scripts/components/login.jsx
+++ app/scripts/components/login.jsx
@@ -1,34 +1,92 @@
+var $ = require('jquery');
 var React = require ('react');
 
+$.fn.serializeObject = function() {
+    return this.serializeArray().reduce(function(acum, i) {
+      acum[i.name] = i.value;
+      return acum;
+    }, {});
+  };
+
+function setupAjax(user){
+  $.ajaxSetup({
+    beforeSend: function(xhr){
+      xhr.setRequestHeader("X-Parse-Application-Id", "tiygvl");
+      xhr.setRequestHeader("X-Parse-REST-API-Key", "slumber");
+      if(user){
+        xhr.setRequestHeader("X-Parse-Session-Token", user.sessionToken);
+      }
+    }
+  });
+}
+
+setupAjax();
+
+var baseUrl = 'https://tiny-lasagna-server-new.herokuapp.com';
+
 class Login extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  _signup(event) {
+    event.preventDefault();
+    var user = $(event.target).serializeObject();
+    user = {
+      username: user.email,
+      password: user.password
+    };
+    $.post(baseUrl + '/users', user).then(function(data){
+      console.log('session id', data.sessionToken);
+    });
+  }
+
+  _login(event) {
+    event.preventDefault();
+    var user = $(event.target).serializeObject();
+    var username = user.email;
+    var password = user.password;
+
+    var url = baseUrl + '/login?username=' +
+            encodeURIComponent(username) + '&' +
+            'password=' + encodeURIComponent(password);
+
+    $.get(url).then(function(data){
+      var userData = JSON.stringify(data);
+      localStorage.setItem('user', userData);
+      console.log('session id', data.sessionToken);
+    });
+  }
+
   render() {
     return (
       <div className="row">
-         <div className="col-md-6">
+        <div className="col-md-6">
           <h1>Please Login</h1>
-          <form id="login">
-             <div className="form-group">
-               <input className="form-control" name="email" id="email-login" type="email" placeholder="email" />
-             </div>
+          <form id="login" onSubmit={this._login.bind(this)}>
             <div className="form-group">
-               <input className="form-control" name="password" id="password-login" type="password" placeholder="Password Please" />
-             </div>
+              <input className="form-control" name="email" id="email-login" type="email" placeholder="email" />
+            </div>
+            <div className="form-group">
+              <input className="form-control" name="password" id="password-login" type="password" placeholder="password" />
+            </div>
             <input className="btn btn-primary" type="submit" value="Login" />
           </form>
-         </div>
+        </div>
         <div className="col-md-6">
           <h1>No Account Please Sign Up</h1>
-          <form id="signup">
-           <div className="form-group">
-             <input id="signup-email" className="form-control" type="text" name="email" placeholder="Email Address" />
-           </div>
-           <div className="form-group">
-             <input id="signup-password" className="form-control" type="text" name="password" placeholder="Password" />
-           </div>
-           <input className="btn btn-primary" type="submit" name="" value="Sign Up!" />
+          <form id="signup" onSubmit={this._signup.bind(this)}>
+            <div className="form-group">
+              <input className="form-control" name="email" id="email-login" type="email" placeholder="email" />
+            </div>
+            <div className="form-group">
+              <input id="signup-password" className="form-control" type="text" name="password" placeholder="password" />
+            </div>
+            <input className="btn btn-primary" type="submit" name="" value="Sign Up!" />
           </form>
-         </div>
-       </div>
+        </div>
+      </div>
     )
   }
 }
